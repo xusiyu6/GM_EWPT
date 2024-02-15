0787b4cd096bc440c2dc695801db7a8d5da6aff9
Yongcheng Wu <yc_wu1030@126.com>
2024-02-16 00:43:03 +0800

    impr(GM_Z2_15): zero temperature relations

--- models/GM_Z2_15.py
+++ models/GM_Z2_15.py
@@ -18,7 +18,34 @@ class GM_Z2_15(generic_potential.generic_potential):
     """
     The effective potential for Z2 symmetric GM model with v1 and v8=\sqrt{2}v5 (neglecting other background fields)
     """
-    def init(self):
-        pass
+    def init(self, sH, sa, MHH, MH3, MH5):
+        self.set_physical_parameters(sH,sa,MHH,MH3,MH5)
 
-    def set_potential_parameters(self, mu22, mu32, lam1, lam2)
+    def set_physical_parameters(self, sH, sa, MHH, MH3, MH5):
+        if sH > 1:
+            sH = 1
+        if sH < -1:
+            sH = -1
+        self.sH = sH
+        self.cH = np.sqrt(1-sH*sH)
+        self.v1 = C_VEV*self.cH
+        self.v5 = C_VEV*self.sH/2.0/np.sqrt(2.0)
+
+        if sa > 1:
+            sa = 1
+        if sa < -1:
+            sa = -1
+        self.sa = sa
+        self.ca = np.sqrt(1-sa*sa)
+        self.MHL = C_MH
+        self.MHH = MHH
+        self.MH3 = MH3
+        self.MH5 = MH5
+        self.lam1 = (self.MHL**2*self.ca**2+self.MHH**2*self.sa**2)/8.0/self.v1**2
+        self.lam2 = self.MH3**3/C_VEV**2 + self.sa*self.ca/4.0/np.sqrt(3)*(self.MHH**2-self.MHL**2)/self.v1/self.v5
+        self.lam3 = self.MH5**2/8.0/self.v5**2 - 3.0*self.v1**2/8.0/self.v5**2*self.MH3**2/C_VEV**2
+        self.lam4 = self.v1**2/8.0/self.v5**2*self.MH3**2/C_VEV**2 + (self.MHL**2*self.sa**2+self.MHH**2*self.ca**2-self.MH5**2)/24.0/self.v5**2
+        self.lam5 = 2.0*self.MH3**2/C_VEV**2
+
+        self.mu22 = -4.0*self.lam1*self.v1**2 + 3.0*(self.lam5-2.0*self.lam2)*self.v5**2
+        self.mu32 = (self.lam5-2.0*self.lam2)*self.v1**2 - 4.0*(self.lam3+3.0*self.lam4)*self.v5**2
