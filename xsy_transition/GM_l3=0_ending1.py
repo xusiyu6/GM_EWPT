80a21cfc29a5bc061f1d8080affaf58fcd29d9dc
Yongcheng Wu <yc_wu1030@126.com>
2024-04-15 13:21:03 +0800

    impr(GM_l3=0): improve the scan

--- xsy_transition/GM_l3=0_ending1.py
+++ xsy_transition/GM_l3=0_ending1.py
@@ -1,15 +1,16 @@
 import numpy as np
 from cosmoTransitions import generic_potential
 import random
+import time
 import os
 
 # 改变输出位置。导入了sys模块。
-import sys
+# import sys
 
-file = open('result5.txt', 'a+')
+# file = open('result5.txt', 'a+')
 # # a+是每次输出直接累加，w+是覆盖上一次的结果，输出新的结果。
 # # 创建了一个名为file的文件对象，打开名为result.txt的文件以进行附加写入（a+模式）。
-sys.stdout = file
+# sys.stdout = file
 # 输出到result.txt。
 # 接下来，它将标准输出sys.stdout重定向到这个打开的文件对象上，这意味着所有后续的标准输出都将被写入到result.txt文件中。
 # sys.stdout代表“标准输出”的属性。通过将这个属性的值修改为某个文件对象，可以将本来要打印到屏幕上的内容写入文件。
@@ -50,6 +51,49 @@ class GM_Z2_15(generic_potential.generic_potential):
         self.lam5 = 2 * (self.M5 ** 2 - self.M1 ** 2) / self.v ** 2
         self.mu22 = -4.0 * self.lam1 * self.v ** 2
         self.mu32 = (self.M1 ** 2 + 2 * self.M5 ** 2 - 4 * self.lam2 * self.v ** 2) / 2
+        self.c2 = (3.0*self.g**2+self.gp**2)/16.0 + self.yt**2/4.0 + 2.0*self.lam1 + 1.5*self.lam2
+        self.c3 = (3.0*self.g**2+self.gp**2)/2.0 + 2.0*self.lam2 + 11.0*self.lam4
+
+    def check_EWPT_critical(self):
+        if self.c3/self.c2 >= 3.0*self.mu32/self.mu22:
+            return False
+        if 3.0*self.mu32/self.mu22 >= 3.0*np.sqrt(self.lam4)/np.sqrt(self.lam1):
+            return False
+        if 3.0*np.sqrt(self.lam4)/np.sqrt(self.lam1) >= 3.0*(2.0*self.lam2 - self.lam5)/4.0/self.lam1:
+            return False
+        return True;
+
+    def check_UNI(self, method = 0): # Checking whether the Unitarity conditions are satisfied
+        # Method 1: |a0|<1;
+        # Method 0: |Rea0|<1/2;
+        factor=1
+        if method == 1:
+            factor=2
+        if np.sqrt((6.0*self.lam1 - 11.0*self.lam4)**2+36.0*self.lam2**2) + np.abs(6.0*self.lam1 + 11.0*self.lam4) >= 4.0*factor*np.pi:
+            return False
+        if np.sqrt((2.0*self.lam1-2.0*self.lam4)**2+self.lam5**2)+np.abs(2.0*self.lam1+2.0*self.lam4) >= 4.0*factor*np.pi:
+            return False
+        if np.abs(self.lam4) >= factor*np.pi:
+            return False
+        if np.abs(self.lam2-self.lam5) >= factor*2*np.pi:
+            return False
+        return True
+    def check_BFB(self): # Checking whether the BFB conditions are satisfied
+        # Works only for lam3 = 0
+        if self.lam1 <= 0:
+            return False
+        if self.lam4 <= 0:
+            return False
+        if self.lam5>=0 and self.lam2 <= self.lam5/2.0 - 2.0*np.sqrt(self.lam1*self.lam4):
+            return False
+        if self.lam5<0 and self.lam2 <= -self.lam5/4.0 - 2.0*np.sqrt(self.lam1*self.lam4):
+            # This only works for lam3 = 0
+            # where we only need to check for zeta = 1/2 where omega_minus obtain its minimum negative value
+            return False
+        return True
+
+    def Tc_Analytical(self):
+        return np.sqrt((3.0*self.mu22*np.sqrt(self.lam4)-3.0*self.mu32*np.sqrt(self.lam1))/(self.c3*np.sqrt(self.lam1)-3.0*self.c2*np.sqrt(self.lam4)))
 
     def V0(self, X):  # 重写generic_potential里的方法V0
         X = np.asanyarray(X)
@@ -69,11 +113,12 @@ class GM_Z2_15(generic_potential.generic_potential):
         omg5 = X[..., 1]
 
         y = self.V0(X)
-        y += 1 / 6 * self.mu22 * T ** 2 + 3 / 8 * self.mu32 * T ** 2 + self.lam1 * omg1 ** 2 * T ** 2 + 3 / 4 * self.lam2 * omg1 ** 2 * T ** 2
-        y += self.lam2 * omg5 ** 2 * T ** 2 + 11 / 2 * self.lam4 * omg5 ** 2 * T ** 2
-        y += 1 / 16 * self.g ** 2 * (omg1 ** 2 + 8 * omg5 ** 2) * T ** 2 + 1 / 32 * (self.g ** 2 + self.gp ** 2) * (
-                omg1 ** 2 + 8 * omg5 ** 2) * T ** 2
-        y += 1 / 8 * self.yt ** 2 * omg1 ** 2 * T ** 2
+        y += self.c2*omg1**2*T**2/2.0 + self.c3*omg5**2*T**2/2.0
+        # y += 1 / 6 * self.mu22 * T ** 2 + 3 / 8 * self.mu32 * T ** 2 + self.lam1 * omg1 ** 2 * T ** 2 + 3 / 4 * self.lam2 * omg1 ** 2 * T ** 2
+        # y += self.lam2 * omg5 ** 2 * T ** 2 + 11 / 2 * self.lam4 * omg5 ** 2 * T ** 2
+        # y += 1 / 16 * self.g ** 2 * (omg1 ** 2 + 8 * omg5 ** 2) * T ** 2 + 1 / 32 * (self.g ** 2 + self.gp ** 2) * (
+        #         omg1 ** 2 + 8 * omg5 ** 2) * T ** 2
+        # y += 1 / 8 * self.yt ** 2 * omg1 ** 2 * T ** 2
         return y
 
     def approxZeroTMin(self):  # 重写generic_potential里的方法approxZeroTMin
@@ -148,48 +193,79 @@ def makePlots(M1, M5, lam2, lam4, m=None):
         # 将m.calcTcTrans()的返回值返回到makePlots外
         return m.findAllTransitions()
 
-nn = 1
-while nn <= 100:
+def GM_EWPT_Scan(filename,n_required=500):
+    with open(filename,'w') as f:
+        n_obtained = 0
+        while n_obtained < n_required:
+            M1_rnd = generate_random_number(0,2000)
+            M5_rnd = generate_random_number(0,2000)
+            lam2_rnd = generate_random_number(-3,3)
+            lam4_rnd = generate_random_number(0,3.2)
 
-    M1_totest = generate_random_number(0, 2000)
-    M5_totest = generate_random_number(0, 2000)
-    lam2_totest = generate_random_number(-3,3)
-    lam4_totest = generate_random_number(0,3)
-    # 待测试的M1_totest, M5_totest, lam2_totest, lam4_totest已经生成，为了区分送到GM_Z2_15的参数，加了后缀_totest
+            mod = GM_Z2_15(M1_rnd,M5_rnd,lam2_rnd,lam4_rnd)
+            if not mod.check_UNI() or not mod.check_BFB() or not mod.check_EWPT_critical():
+                continue
+            Tc_analytical = mod.Tc_Analytical()
+            nucl_res = mod.findAllTransitions()
+            for nucl in nucl_res:
+                nucl_type = nucl['trantype']
+                if nucl_type != 1:
+                    continue
+                Tn = nucl['Tnuc']
+                crit_res = nucl['crit_trans']
+                Tc = crit_res['Tcrit']
+                n_obtained += 1
+                f.write("%f %f %f %f %f %f %f"%(M1_rnd,M5_rnd,lam2_rnd,lam4_rnd,Tn,Tc,Tc_analytical))
 
-    if eqtest(M1_totest, M5_totest, lam2_totest, lam4_totest) == 0:  # 测试M1, M5, lam2, lam4
-            continue  # 测试未通过则重新取随机数
-    else:  # 通过则打印4个随机数并调用makePlots
 
-        sys.stdout = open(os.devnull, 'w')
-
-        result = makePlots(M1_totest, M5_totest, lam2_totest, lam4_totest)
-            # 恢复标准输出
-        sys.stdout = file
-        # sys.stdout = sys.__stdout__
-
-        if result:  # 检查列表是否为空
-            # for k in range(1, len(result)):
-                T=result[len(result) - 1]
-                if T is not None:
-                   Tnucvalue = T['Tnuc']
-                   Tc = T['crit_trans']
-                   Ttrantype=T['trantype']
-                   if Tc is not None:
-                       Tcritvalue = Tc['Tcrit']
-                       Tctrantype=Tc['trantype']
-                       if Tctrantype==1:
-                           print(M1_totest, M5_totest, lam2_totest, lam4_totest, Tnucvalue, Tcritvalue, T['trantype'],
-                                 Tc['trantype'])
-
-                           nn += 1
-                       else:
-                           continue
+if __name__ == "__main__":
+    timestamp = time.strftime("%Y%m%d%H%M%S",time.localtime())
+    GM_EWPT_Scan("GM_EWPT_Scan_%s.dat"%timestamp)
 
 
 
+# nn = 1
+# while nn <= 100:
+
+#     M1_totest = generate_random_number(0, 2000)
+#     M5_totest = generate_random_number(0, 2000)
+#     lam2_totest = generate_random_number(-3,3)
+#     lam4_totest = generate_random_number(0,3)
+#     # 待测试的M1_totest, M5_totest, lam2_totest, lam4_totest已经生成，为了区分送到GM_Z2_15的参数，加了后缀_totest
+
+#     if eqtest(M1_totest, M5_totest, lam2_totest, lam4_totest) == 0:  # 测试M1, M5, lam2, lam4
+#             continue  # 测试未通过则重新取随机数
+#     else:  # 通过则打印4个随机数并调用makePlots
+
+#         sys.stdout = open(os.devnull, 'w')
+
+#         result = makePlots(M1_totest, M5_totest, lam2_totest, lam4_totest)
+#             # 恢复标准输出
+#         sys.stdout = file
+#         # sys.stdout = sys.__stdout__
+
+#         if result:  # 检查列表是否为空
+#             # for k in range(1, len(result)):
+#                 T=result[len(result) - 1]
+#                 if T is not None:
+#                    Tnucvalue = T['Tnuc']
+#                    Tc = T['crit_trans']
+#                    Ttrantype=T['trantype']
+#                    if Tc is not None:
+#                        Tcritvalue = Tc['Tcrit']
+#                        Tctrantype=Tc['trantype']
+#                        if Tctrantype==1:
+#                            print(M1_totest, M5_totest, lam2_totest, lam4_totest, Tnucvalue, Tcritvalue, T['trantype'],
+#                                  Tc['trantype'])
+
+#                            nn += 1
+#                        else:
+#                            continue
 
 
 
 
-file.close()
+
+
+
+# file.close()
