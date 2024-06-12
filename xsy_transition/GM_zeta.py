99ff8c9030d8bd4a301e5b52e5e03428bdebf6c4
xusiyu6 <128487888+xusiyu6@users.noreply.github.com>
2024-06-12 22:38:47 +0800

    Update and rename GM_zeta=1_2.py to GM_zeta.py
    不限制zeta取值

--- xsy_transition/GM_zeta.py
+++ xsy_transition/GM_zeta.py
@@ -16,10 +16,13 @@ C_MB = 4.18
 C_MH = 125.0
 C_PI = 3.1415927
 
+def omega(zeta, B):
+    omega_plus = 1 / 6 * (1 - B) + np.sqrt(2) / 3 * np.sqrt((1 - B) * (1 / 2 + B))
+    omega_minus = 1 / 6 * (1 - B) - np.sqrt(2) / 3 * np.sqrt((1 - B) * (1 / 2 + B))
+    return omega_plus, omega_minus
 
 class GM_Z2_15(generic_potential.generic_potential):
-
-    def __init__(self, M1, M5, lam2, lam3, lam4):  # 重写generic_potential里的方法init
+    def __init__(self, M1, M5, lam2, lam3, lam4):
         self.Ndim = 2
         self.renormScaleSq = C_VEV ** 2
         self.g = 2.0 * C_MW / C_VEV
@@ -45,6 +48,32 @@ class GM_Z2_15(generic_potential.generic_potential):
         self.mu22 = -4.0 * self.lam1 * self.v ** 2
         self.mu32 = (self.M1 ** 2 + 2 * self.M5 ** 2 - 6 * self.lam2 * self.v ** 2) / 3
 
+    def check_BFB(self):  # Checking whether the BFB conditions are satisfied
+        if self.lam1 <= 0:
+            return False
+        if self.lam3 >= 0 and self.lam4 <= -1 / 3 * self.lam3:
+            return False
+        if self.lam3 < 0 and self.lam4 <= -self.lam3:
+            return False
+
+        if self.lam5 >= 0:
+            if self.lam3 >= 0 and self.lam2 <= self.lam5 / 2.0 - 2.0 * np.sqrt(self.lam1 * (1 / 3 * self.lam3 + self.lam4)):
+                return False
+            if self.lam3 < 0:
+                for zeta in np.arange(1 / 3, 1, 0.001):
+                    B = np.sqrt(3 / 2 * (zeta - 1 / 3))
+                    omega_plus, _ = omega(zeta, B)
+                    if self.lam2 <= omega_plus * self.lam5 - 2.0 * np.sqrt(self.lam1 * (zeta * self.lam3 + self.lam4)):
+                        return False
+
+        if self.lam5 < 0:
+            for zeta in np.arange(1 / 3, 1, 0.001):
+                B = np.sqrt(3 / 2 * (zeta - 1 / 3))
+                _, omega_minus = omega(zeta, B)
+                if self.lam2 <= omega_minus * self.lam5 - 2.0 * np.sqrt(self.lam1 * (zeta * self.lam3 + self.lam4)):
+                    return False
+        return True
+
     def check_UNI(self, method=0):  # Checking whether the Unitarity conditions are satisfied
         factor = 1
         if method == 1:
@@ -59,32 +88,17 @@ class GM_Z2_15(generic_potential.generic_potential):
             return False
         return True
 
-    def check_BFB(self):  # Checking whether the BFB conditions are satisfied
-        if self.lam1 <= 0:
-            return False
-        if self.lam3 >= 0 and self.lam4 <= -1 / 3 * self.lam3:
-            return False
-        if self.lam3 < 0 and self.lam4 <= -self.lam3:
-            return False
-        if self.lam5 >= 0 and self.lam3 >= 0 and self.lam2 <= self.lam5 / 2.0 - 2.0 * np.sqrt(self.lam1 * (1 / 3 * self.lam3 + self.lam4)):
-            return False
-        if self.lam5 >= 0 and self.lam3 < 0 and self.lam2 <= self.lam5 * 5.0 / 12.0 - 2.0 * np.sqrt(self.lam1 * (1 / 2 * self.lam3 + self.lam4)):
-            return False
-        if self.lam5 < 0 and self.lam2 <= -self.lam5 / 4.0 - 2.0 * np.sqrt(self.lam1 * (1 / 2 * self.lam3 + self.lam4)):
-            return False
-        return True
-
 
 def generate_random_number(range_start, range_end):
     return random.uniform(range_start, range_end)
 
 
-def GM_EWPT_Scan(filename, n_required=70000):
+def GM_EWPT_Scan(filename, n_required=500):
     with open(filename, 'w') as f:
         n_obtained = 0
         while n_obtained < n_required:
-            M1_rnd = generate_random_number(0, 800)
-            M5_rnd = generate_random_number(0, 800)
+            M1_rnd = generate_random_number(40, 100)
+            M5_rnd = generate_random_number(40, 800)
             lam2_rnd = generate_random_number(-5, 5)
             lam3_rnd = generate_random_number(-5, 5)
             lam4_rnd = generate_random_number(-5, 5)
@@ -100,4 +114,4 @@ def GM_EWPT_Scan(filename, n_required=70000):
 
 if __name__ == "__main__":
     timestamp = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    GM_EWPT_Scan("2zeta2GM_EWPT_Scan_%s.dat"%timestamp)
+    GM_EWPT_Scan("1zetaGM_%s.dat" % timestamp)
